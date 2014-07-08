4c334b1bb26a3e05722af783b25692745a09764d
ddwooten <ddwooten@rocketmail.com>
2014-07-09 00:50:33 +0200

    Added csv reader functionality. Still figuring out how it works

--- SIP/SERP_mat_conditioner.py
+++ SIP/SERP_mat_conditioner.py
@@ -1,5 +1,8 @@
 # Creator: Daniel Wooten
 # Version 1.0.
+
+import csv
+
 """ SERP_mat_conditioner is a simple python script that takes in a tab delimited input
     file which contains parameters and options and outputs the appropriate fuel
     composition in atomic percent inside of a SERPENT file that is also specified in the
@@ -13,4 +16,12 @@
     The < CAFH > flag determines how the < Abundance > value is handled. This will be
     made more clear in the body of the code through comments and doc strings """
 
+print "Fuel Material Conditioner beginning"
+inputfile = raw_input( "Please enter file name, local path only, of csv file to open \n" )
+print "Opening csv file now"
+csvfile = open( inputfile, "r" )
+reader = csv.reader( csvfile )
+print reader(1)
+
+exit()
 
