6976ad52a7aefdec3400d340a5aee5cac45d1585
ddwooten <ddwooten@rocketmail.com>
2014-07-08 23:51:56 +0200

    Added the excel sheet along with generated txt and csv files for testing of SIP_Sample_Input_Test_1

--- SIP/SERP_mat_conditioner.py
+++ SIP/SERP_mat_conditioner.py
@@ -1,1 +1,16 @@
-place holder
+# Creator: Daniel Wooten
+# Version 1.0.
+""" SERP_mat_conditioner is a simple python script that takes in a tab delimited input
+    file which contains parameters and options and outputs the appropriate fuel
+    composition in atomic percent inside of a SERPENT file that is also specified in the
+    input file. A sample input file is given below
+    <<
+    < Combined/Seperate >
+    < Free/Preserved >
+    < Path_to_SERPENT_input_file.txt >
+    < A of isotope n , Z of n , < C , A , F , H >, BNC , BNH , Abudance >
+    >>
+    The < CAFH > flag determines how the < Abundance > value is handled. This will be
+    made more clear in the body of the code through comments and doc strings """
+
+
