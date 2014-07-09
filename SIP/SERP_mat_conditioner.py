003e3b3e9faa4b87835914a3396f92e0942d0384
ddwooten <ddwooten@rocketmail.com>
2014-07-09 16:35:21 +0200

    Modified test.csv input file, changed order of fields and illiminated non integer values from numerical arrays. In SERP file is now read in and the numerical data is placed into a new array to give it type float for arrithmatic.

--- SIP/SERP_mat_conditioner.py
+++ SIP/SERP_mat_conditioner.py
@@ -38,6 +38,29 @@ for row in reader:
 for row in range( len ( csvinput ) ):
   print csvinput[ row ]
 
+# initilizing FloatsInput array, the next 11 lines of code simply copy the csvinput
+# array into another array as float values as opposed to strings
+
+FloatsInput=[]
+csvinputrows = len( csvinput )
+floatrows = csvinputrows - 3
+print " Creating floats array"
+for i in range( floatrows ):
+  FloatsInput.append( 0 )
+for row in range( 3 , csvinputrows ):
+  tmpArray=[]
+  for column in range( len( csvinput[ row ] ) ):
+    if len( csvinput[ row ][ column ] ) > 0:
+      tmpArray.append( float( csvinput[ row ][ column ] ) )
+  FloatsInput[ row - 3 ] = tmpArray
+for i in range( floatrows ):
+  print FloatsInput[ i ]
+
+if csvinput[ 0 ][ 0 ] == "Free" or csvinput[ 0 ][ 0 ] == "free" \
+    or csvinput[ 0 ][ 0 ] == "FREE":
+    print "Running under the \"Free\" assumption"
+    molcar = 1.0 -float( csvinput[ 2 ][ 0 ] ) -float( csvinput[ 2 ][ 1 ] )
+    saltarray = []
 
 print "The SERPENT material conditioner had finished running"
 exit()
