e9c8053bd5250979f997e82801442ccd6a8899ca
ddwooten <ddwooten@rocketmail.com>
2014-07-09 13:46:17 +0200

    added test.csv which is a copy of the SIP csv file with a shorter file name. Additionally, now have csv file being read into python with the csv module. Currently stored as a list of lists composed of strings.

--- SIP/SERP_mat_conditioner.py
+++ SIP/SERP_mat_conditioner.py
@@ -1,7 +1,8 @@
 # Creator: Daniel Wooten
 # Version 1.0.
 
-import csv
+import csv as csvreader
+import numpy as np
 
 """ SERP_mat_conditioner is a simple python script that takes in a tab delimited input
     file which contains parameters and options and outputs the appropriate fuel
@@ -17,11 +18,27 @@ import csv
     made more clear in the body of the code through comments and doc strings """
 
 print "Fuel Material Conditioner beginning"
-inputfile = raw_input( "Please enter file name, local path only, of csv file to open \n" )
-print "Opening csv file now"
-csvfile = open( inputfile, "r" )
-reader = csv.reader( csvfile )
-print reader(1)
 
+inputfile = raw_input( "Please enter file name, local path only, of csv file to open \n" )
+
+print "Opening csv file now"
+
+csvfile = open( inputfile, "r" )
+
+print "Reading file now"
+
+reader = csvreader.reader( csvfile )
+
+# initilizing array
+csvinput = []
+
+for row in reader:
+  csvinput.append( row )
+
+for row in range( len ( csvinput ) ):
+  print csvinput[ row ]
+
+
+print "The SERPENT material conditioner had finished running"
 exit()
 
