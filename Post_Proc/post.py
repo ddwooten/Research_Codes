04db9361b17c74ee26b058a8efb959027a8cc21e
ddwooten <ddwooten@rocketmail.com>
2015-04-04 16:09:34 -0700

    Successfully implemented wc module

--- Post_Proc/post.py
+++ Post_Proc/post.py
@@ -7,6 +7,7 @@ import time as time
 import logging as logging
 import copy as cp
 import re as re
+import wooten_common as wc
 
 """ This is the SERPENT post processor. It requires a configuration
     file titled, exactly, "post_setup.txt". 
@@ -22,34 +23,16 @@ import re as re
             it defaults to 0 
         [log_name] - the base name for the log file
 """
-def Read_Setup():
-    """ This function reads in a file named "post_setup.txt". It stores this 
-    file as a list object from which the file's contents can be retrieved."""
-    input_file = open( "post_setup.txt" , "r" )
-    setup_file = input_file.readlines()
-    setup_file = [ x.rstrip( "\n" ) for x in setup_file ]
-    dictionary = {}
-    num_lines = len( setup_file )
-    for i in range( num_lines ):
-        line = setup_file[ i ].split( ',' )
-        dictionary[ line[ 0 ] ] = line[ 1 ]
-    if 'log_level' in dictionary.keys():
-        dictionary[ 'log_level' ] = int( dictionary[ 'log_level' ] )
-    input_file.close()
-    return( dictionary )
 
-def Nuclide_Dictionaries( Sep , Cep ):
+def Nuclide_Dictionaries():
     """ This function reads in nuclide ZAIs and names from a static file,
     "nuclide_ids.txt" and creates a dictionary of name:ZAI and ZAI:name """
-    Sep()
+    wc.Sep()
     logging.debug( "Nuclide_Dictionaries" )
-    input_file = open( "nuclide_ids.txt" , "r" )
-    setup_file = input_file.readlines()
-    setup_file = [ x.rstrip( "\n" ) for x in setup_file ]
+    nuclide_file = wc.Read_Input( "nuclide_ids.txt" , "r" )
     dictionary_0 = {}
     dictionary_1 = {}
-    num_lines = len( setup_file )
-    for i in range( num_lines ):
+    for i in range( len( nuclide_file ) ):
         line = setup_file[ i ].split( ':' )
         dictionary_0[ line[ 0 ] ] = line[ 1 ]
         dictionary_1[ line[ 1 ] ] = line[ 0 ]
@@ -57,18 +40,18 @@ def Nuclide_Dictionaries( Sep , Cep ):
     output_list = [ dictionary_0 , dictionary_1 ]
     return( output_list )
 
-def Read_Burn_File( base_name, options , Read_Input , Get_Materials_List , \
-    Get_Nuclide_Indicies , Sep , Cep ):
+def Read_Burn_File( base_name, options , Get_Materials_List , \
+    Get_Nuclide_Indicies ):
     """ This function reads in a SERPENT2 ( Aufiero and later mod ) burnup file
         and stores data as lists inside of a dictionary """
-    Sep()
+    wc.Sep()
     logging.debug( "Read_Burn_File" )
     burn_data = {} 
     nuclide_indicies = {}
-    raw_burn_data = Read_Input( base_name + "_dep.m" , "string" , Sep )
+    raw_burn_data = wc.Read_Input( base_name + "_dep.m" , "string" )
     raw_burn_data = [ x.strip( " " ) for x in raw_burn_data ]
-    materials_list = Get_Materials_List( raw_burn_data , options , Sep , Cep )
-    Cep()
+    materials_list = Get_Materials_List( raw_burn_data , options )
+    wc.Cep()
     index_pattern = re.compile( r'i(\d*) = (\d*)' )
     bu_pattern = re.compile( r'BU' )
     day_pattern = re.compile( r'DAYS' )
@@ -95,30 +78,30 @@ def Read_Burn_File( base_name, options , Read_Input , Get_Materials_List , \
         if index_match:
             logging.info( "Index match!" )
             nuclide_indicies = Get_Nuclide_Indicies( line ,\
-                nuclide_indicies ,Sep, Cep )
+                nuclide_indicies )
             i += 1 
         elif bu_match:
             vectors += 1
             logging.info( "BU match!" )
             if vector_match:
                 burn_data[ bu_match.group() ] = Parse_Matlab_Vector( \
-                    line , Sep , Cep )
+                    line )
                 i += 1 
             else:
-                start , i = Get_Matlab_Matrix( raw_burn_data , i , Sep , Cep )
+                start , i = Get_Matlab_Matrix( raw_burn_data , i )
                 burn_data[ bu_match.group() ] = Parse_Matlab_Matrix( \
-                    start, i , raw_burn_data , Sep , Cep )
+                    start, i , raw_burn_data )
         elif day_match:
             vectors += 1
             logging.info( "Day match!" )
             if vector_match:
                 burn_data[ day_match.group() ] = Parse_Matlab_Vector(\
-                    line , Sep , Cep )
+                    line )
                 i += 1
             else:
-                start , i = Get_Matlab_Matrix( raw_burn_data , i , Sep , Cep )
+                start , i = Get_Matlab_Matrix( raw_burn_data , i )
                 burn_data[ day_match.group() ] = Parse_Matlab_Matrix( \
-                    start, i , raw_burn_data , Sep , Cep )
+                    start, i , raw_burn_data )
         elif mat_match or tot_match:
             vectors += 1
             logging.info( "Data match!" )
@@ -129,23 +112,23 @@ def Read_Burn_File( base_name, options , Read_Input , Get_Materials_List , \
             logging.info( "Data is: " + field_name )
             if vector_match:
                 burn_data[ field_name ] = Parse_Matlab_Vector(\
-                    line , Sep , Cep )
+                    line )
                 i += 1
             else:
-                start , i = Get_Matlab_Matrix( raw_burn_data , i , Sep , Cep )
+                start , i = Get_Matlab_Matrix( raw_burn_data , i )
                 burn_data[ field_name ] = Parse_Matlab_Matrix( \
-                    start, i , raw_burn_data , Sep , Cep )
+                    start, i , raw_burn_data )
         else:
             i += 1
     logging.info( "The number of vectors is: " + str( vectors ) )
     output = [ burn_data , nuclide_indicies , materials_list ]
     return( output )
 
-def Get_Matlab_Matrix( contents , counter , Sep , Cep ):
+def Get_Matlab_Matrix( contents , counter ):
     """ This function searches through a serpent dep file and extracts
         the currently selected matlab style matrix ( composing several lines
         of the file ) returning this matrix as a list """
-    Sep()
+    wc.Sep()
     logging.debug( "Get_Matlab_Matrix" )
     start = cp.deepcopy( counter )
     pattern = re.compile( r'\];' )
@@ -164,10 +147,10 @@ def Get_Matlab_Matrix( contents , counter , Sep , Cep ):
     output = [ start , counter ]
     return( output ) 
 
-def Parse_Matlab_Matrix( begin , end , contents , Sep , Cep ):
+def Parse_Matlab_Matrix( begin , end , contents ):
     """ This function extracts numerical data from a matlab matrix and returns
         a nested list of the data"""
-    Sep()
+    wc.Sep()
     logging.debug( "Parse_Matlab_Matrix" )
     output = []
     for i in range( begin + 1 , end ):
@@ -186,10 +169,10 @@ def Parse_Matlab_Matrix( begin , end , contents , Sep , Cep ):
         output.append( line )
     return( output )
 
-def Parse_Matlab_Vector( line , Sep , Cep ):
+def Parse_Matlab_Vector( line ):
     """ This function extracts the numerical data from a matlab vector format
         and returns a list """
-    Sep()
+    wc.Sep()
     logging.debug( "Parse_Matlab_Vector" )
     pattern = re.compile( r'\[.*?\]' )
     string = pattern.search( line ).group()[ 1 : -1 ].strip( " " )
@@ -206,10 +189,10 @@ def Parse_Matlab_Vector( line , Sep , Cep ):
         logging.debug( str( nums ) )
     return( nums )
 
-def Get_Nuclide_Indicies( string , index_dict , Sep , Cep ):
+def Get_Nuclide_Indicies( string , index_dict ):
     """ This function reads through the burn file to get the matrix rows of
         each nuclide ( the index in matricies where it's info can be found ) """
-    Sep()
+    wc.Sep()
     logging.debug( "Get_Nuclide_Indicies" )
     logging.debug( "The string being analyzed is:" )
     logging.debug( string )
@@ -221,9 +204,9 @@ def Get_Nuclide_Indicies( string , index_dict , Sep , Cep ):
     logging.debug( match.group( 1 ) + " : " + match.group( 2 ) ) 
     return( index_dict )
 
-def Get_Materials_List( contents , options , Sep , Cep ):
+def Get_Materials_List( contents , options ):
     """ This function generates a list of all the materials in the burn file"""
-    Sep()
+    wc.Sep()
     logging.debug( "Get_Materials_List" )
     materials = []
     pattern = re.compile(r'MAT_(\S*?)_VOLUME')
@@ -240,7 +223,7 @@ def Get_Materials_List( contents , options , Sep , Cep ):
 
 def Report_Output( output , file_name ):
     """ This function is a debugging function to test the parse functions """
-    Sep()
+    wc.Sep()
     logging.debug( "Report_Output" )
     destination = open( file_name , "w" )
     for key in output.keys():
@@ -263,88 +246,32 @@ def Report_Output( output , file_name ):
     destination.write( "********************************************* \n" )
     destination.write( "END!!" )
     destination.close()
-
-def Get_Base_Name( file_name ):
-    """ This function gets a base name from the host file name """
-    end_index = file_name.rfind( "." )
-    base_name = file_name[ 0 : end_index ]
-    return( base_name )
-
-def Start_Log( base_name , level ): 
-    log_file_name = "Log_" + base_name + "_" + time.strftime( "%d_%m_%Y" ) \
-        + ".log"
-
-    LogLevel = level 
-
-    logging.basicConfig( filename = log_file_name , format = \
-        "[%(levelname)8s] %(message)s" , filemode = 'w' , level = LogLevel )
-    logging.debug( "This is the debug level reporting in" )
-    logging.info( "This is the info level reporting in " )
-    logging.warning( "This is the warning level reporting in" )
-    logging.error( "This is the error level reporting in" )
-    logging.critical( "This is the critical level reporting in" )
-
-def Cep():
-	''' A wrapper to place file seperators in a log file for the
-	debug level '''
-	logging.debug( "*****************************************************" )
-	return()
-
-#Function, refer to docstring for purpose
-def Sep():
-    '''A wrapper to visually seperate functions in log files'''
-    logging.debug( '//////////////////////////////////////////////////////' )
-    return()
-
-def Read_Input( file_name , form , Sep ):
-    """ This function reads in a file whose name is given in file_name to the
-    function. It's contents are saved in a list and stripped of new lines. 
-    They are also converted to floats. """
-    Sep()
-    logging.debug( "Read_Input" )
-    logging.debug( "Reading in file: " + file_name )
-    input_file = open( file_name , "r" )
-    file_contents = input_file.readlines()
-    if form == 'string':
-        file_contents = [ x.rstrip( "\n" ) for x in file_contents ]
-    elif form == 'float':
-        file_contents = [ float( x ) for x in file_contents ]
-    elif form == 'raw':
-        pass
-    else:
-        print( "ERROR!!!: Choice of conversion for input from file " + \
-            file_name + " either not given or not 'string','raw', or 'float'" )
-        logging.debug( "ERROR!!!: Choice of conversion for input from file " + \
-            file_name + " either not given or not 'string', 'raw', or 'float'" )
-        exit()
-    logging.debug( "Closing file: " + file_name )
-    input_file.close()
-    return( file_contents )
+    return
 
 # Start the program
 
 def Post_Main():
     """ This function runs the program if it is called as an import """
-    setup = Read_Setup()
+    setup = wc.Read_Setup( "post" )
 
     try:
-        Start_Log( setup[ 'log_name' ] ,  setup[ "log_level" ] )
+        wc.Start_Log( setup[ 'log_name' ] ,  setup[ "log_level" ] )
     except:
-        Start_Log( 'post_error' , 0 )
+        wc.Start_Log( 'post_error' , 0 )
         logging.debug( "ERROR!!: < log_level > or < log_name > was not \n \
             found in post_setup.txt and as such LogLevel was set to 0 \n \
             and the base name to 'post_error' " )
 
     if 'log_level' in setup:
         if setup[ 'log_level' ] < 10:
-            Sep()
+            wc.Sep()
             logging.debug( "The input dictionary is: " )
             for keys,values in setup.items():
                 logging.debug( str( keys ) + " : " + str( values ) )
 
     if os.path.isfile( setup[ "host_file" ] + "_dep.m" ): 
-        output = Read_Burn_File( setup[ "host_file" ] , setup , Read_Input, \
-                    Get_Materials_List , Get_Nuclide_Indicies , Sep , Cep )
+        output = Read_Burn_File( setup[ "host_file" ] , setup , \
+                    Get_Materials_List , Get_Nuclide_Indicies )
 
     Report_Output( output[ 0 ] , "report.test" )
     return( output )
