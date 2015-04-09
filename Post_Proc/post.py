6f3da16389d3dd8ace53f099c08c913387adbbce
ddwooten <ddwooten@rocketmail.com>
2015-04-08 18:13:40 -0700

    Updated the pattern in the nuclide index function

--- Post_Proc/post.py
+++ Post_Proc/post.py
@@ -188,9 +188,9 @@ def Get_Nuclide_Indicies( string , index_dict ):
     logging.debug( "Get_Nuclide_Indicies" )
     logging.debug( "The string being analyzed is:" )
     logging.debug( string )
-    pattern = re.compile( r'i(\d*) = (\d*)' )
+    pattern = re.compile( r'i(\d*)\s*=\s*(\d*)' )
     match = pattern.match( string )
-    if index_match:
+    if match:
         index_dict[ int( match.group( 1 ) ) ] = int( match.group( 2 ) )
     logging.debug( "The key value pair generated is: " )
     logging.debug( match.group( 1 ) + " : " + match.group( 2 ) ) 
